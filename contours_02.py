d395c24e23f2ae7811fe84d95d819cdba433a380
Hans Lösche <58704289+hanslosche@users.noreply.github.com>
2021-06-11 12:22:48 -0400

    Create contours_02.py

---
+++ contours_02.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+img = cv2.pyrDown(cv2.imread('images/hammer.jpg', cv2.IMREAD_UNCHANGED))
+ret, thresh = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY),
+                                         127, 255, cv2.THRESH_BINARY)
+
+contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+for c in contours:
+
+    x, y, w, h = cv2.boundingRect(c)
+    # draw Rectangle
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # calculate coordinates of minimum area of rectangle
+    rect = cv2.minAreaRect(c)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+
+    # draw contours
+    cv2.drawContours(img, [box], 0, (0,0, 255), 3)
+
+    # calculate center and radius 
+    (x, y), radius = cv2.minEnclosingCircle(c)
+    # cast to integers
+    center = (int(x), int(y))
+    radius = int(radius)
+    
+    # draw the circle
+    img = cv2.circle(img, center, radius, (0, 255, 0), 2)
+
+
+cv2.imshow('contours', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
